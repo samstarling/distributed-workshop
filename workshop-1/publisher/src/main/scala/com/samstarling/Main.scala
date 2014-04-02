36c825b39d8840eb266547d2e033c6bec1e08e92
Sam Starling <mail@samstarling.co.uk>
2014-04-02 11:35:02 +0100

    Three workers are now spun up

--- workshop-1/publisher/src/main/scala/com/samstarling/Main.scala
+++ workshop-1/publisher/src/main/scala/com/samstarling/Main.scala
@@ -14,6 +14,7 @@ object Main extends App {
   })
 
   Producer.send("END", "bbc")
+  println("Finished")
 
   def consume(count: Int): Unit = {
     val message = Consumer.read("reply")
--- workshop-1/subscriber/src/main/scala/com/samstarling/Main.scala
+++ workshop-1/subscriber/src/main/scala/com/samstarling/Main.scala
@@ -1,26 +1,34 @@
 package com.samstarling
 
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+
 object Main extends App {
+  val fs = 1 to 3 map { x =>
+    future {
+      val workerName = s"worker${x}"
+      println(s"--> Starting ${workerName}")
+      Worker(workerName).run()
+    }
+  }
 
-  println("--> Subscriber")
+  Thread.sleep(120 * 1000)
+}
 
-  def consume: Unit = {
+case class Worker(name: String) {
+  var count = 0
+
+  def run(): Unit = {
     val message = Consumer.read("bbc")
+    count = count + 1
+    println(s"[${name}] rx: ${count}")
 
     if (message != "END") {
       val isDaniel = message.toString.contains("Daniel")
       if (isDaniel) Producer.send("ACK", "reply")
-      consume
+      run()
     } else {
       Producer.send("END", "reply")
     }
   }
-
-
-  while(true) {
-    println("Consuming...")
-    consume
-    Thread.sleep(1000)
-  }
 }
-

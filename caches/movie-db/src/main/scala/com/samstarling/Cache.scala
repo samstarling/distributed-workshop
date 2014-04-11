3a23e89a5ee0fe9edadb1abd64728a152434dae4
Sam Starling <mail@samstarling.co.uk>
2014-04-11 10:45:57 +0100

    Cache script now hits the cache constantly, until stopped

--- caches/movie-db/src/main/scala/com/samstarling/Cache.scala
+++ caches/movie-db/src/main/scala/com/samstarling/Cache.scala
@@ -4,11 +4,20 @@ import com.zink.cache.CacheFactory
 
 object Cache {
   val cache = CacheFactory.connect()
+  var hits = 0
+  var misses = 0
 
   def getOrPopulate(key: String)(fn: String): String = {
     if(cache.get(key) == null) {
       cache.set(key, fn)
+      misses = misses + 1
+    } else {
+      hits = hits + 1
     }
     cache.get(key).toString
+  }
+
+  override def toString = {
+    s"Hits: ${hits}, misses: ${misses}"
   }
 }
--- caches/movie-db/src/main/scala/com/samstarling/Main.scala
+++ caches/movie-db/src/main/scala/com/samstarling/Main.scala
@@ -7,24 +7,31 @@ import scala.util.Random
 object Main extends App {
   val movies = List(41602, 57158, 110415, 255268)
 
-  1 to 5 foreach { tNo =>
-    future {
-      1 to 5 foreach { lNo =>
-        val result = MovieClient.getPopular
-        println(s"POP - ${result.substring(0, 60)}...")
-        Thread.sleep(1000)
-      }
-    }
-  }
-
-  val x = future {
-    1 to 5 foreach { lNo =>
-      val movie = Random.shuffle(movies).head
-      val result = MovieClient.getDetailById(movie.toString)
-      println(s"MOV - ${movie} - ${result.substring(0, 100)}...")
+  future {
+    while(true) {
+      println(Cache.toString)
       Thread.sleep(1000)
     }
   }
 
-  Thread.sleep(10 * 1000)
+  1 to 5 foreach { tNo =>
+    future {
+      while(true) {
+        val result = MovieClient.getPopular
+        Thread.sleep(500)
+      }
+    }
+  }
+
+  1 to 5 foreach { tNo =>
+    future {
+      1 to 5 foreach { lNo =>
+        val movie = Random.shuffle(movies).head
+        MovieClient.getDetailById(movie.toString)
+        Thread.sleep(500)
+      }
+    }
+  }
+
+  Thread.sleep(60 * 1000)
 }

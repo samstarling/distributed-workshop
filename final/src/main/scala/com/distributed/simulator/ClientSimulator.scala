387a5a7ff3b3f6d3bbf5c96100f4e48dfbfc2757
Sam Starling <mail@samstarling.co.uk>
2014-05-01 11:41:55 +0100

    Separated le files

--- final/src/main/scala/com/distributed/simulator/ClientSimulator.scala
+++ final/src/main/scala/com/distributed/simulator/ClientSimulator.scala
@@ -1,6 +1,6 @@
 package com.distributed.simulator
 
-import com.distributed.{Helpers, MovieServiceImpl}
+import com.distributed.{Logger, Helpers, MovieServiceImpl}
 import scala.concurrent.{ExecutionContext, Await, future, Future}
 import scala.concurrent.duration._
 
@@ -16,43 +16,7 @@ object ClientSimulator {
   }
 }
 
-case class Logger(loggerName: String) {
-  def debug(msg: String) = println(s"${Console.RED}[DEBUG] ${Console.BLUE}$loggerName ${Console.RESET}$msg")
-}
 
-case class Client(name: String) {
-  val movieService = new MovieServiceImpl
-  val logger = new Logger(name)
 
-  import Helpers._
 
-  def run() = {
-    val movies = fetchPopularMovies()
-    logger.debug(s"Got ${movies.size} movies")
-    pickSomeMovies(movies)
-  }
-
-  def fetchPopularMovies() = {
-    movieService.getPopular
-  }
-
-  def pickSomeMovies(movies: Seq[String]) = {
-    movies.foreach { movie =>
-      maybe(0.5)
-        { rateMovie(movie) }
-        { logger.debug(s"Decided not to rate $movie") }
-
-      val sleepyTiemz = (1000 * scala.util.Random.nextFloat).toInt
-      Thread.sleep(sleepyTiemz)
-    }
-  }
-
-  def rateMovie(movie: String) = {
-    if (scala.util.Random.nextFloat() > 0.25) {
-      logger.debug(s"Rating $movie up")
-    } else {
-      logger.debug(s"Rating $movie down")
-    }
-  }
-}
 
